8fef61011c18b4508a0a2c4387ff6c1d15d59084
Sigadong <irwanto.regar30@gmail.com>
2020-10-09 20:08:31 +0700

    Membuat URL Router Sederhana

---
+++ src/scripts/views/pages/upcoming.js
@@ -0,0 +1,13 @@
+const Upcoming = {
+    async render() {
+        return `
+            <h2>Up Coming Page</h2>
+        `;
+    },
+
+    async afterRender() {
+
+    }
+};
+
+export default Upcoming;
